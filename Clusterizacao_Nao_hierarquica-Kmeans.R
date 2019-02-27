62b6e7d74db4020b6e523386a03577606817c8cc
fabiorfc <fabio.rfc@hotmail.com>
2019-02-26 23:21:01 -0300

    Alterando o código de Kmeans

--- Clusterizacao_Nao_hierarquica-Kmeans.R
+++ Clusterizacao_Nao_hierarquica-Kmeans.R
@@ -3,33 +3,45 @@
 #################################################1º) K-MEANS
 
 
-# Determine number of clusters using a function
+# Função para gerar a variabilidade de cada sugestão de cluster
 Cluster.Number = function(x) {
   
-    #Verifying if all vectors are numeric
-    for(i in 1:ncol(x)) {
-      if (is.numeric(x[,1]) == FALSE) {
-        print('All elements from sample vectors must be numeric')
-      }  
-      break
-    }
-      
-    #Calculating the initial within square sum
-    wss = sum(apply(x, 2, var)) * (nrow(x)-1)
-    #Calculus of others SS
-    for(i in 2:(nrow(x)-1)) { wss[i] = sum(kmeans(x, centers = i, iter.max = 500)$withinss) }
-    
-    return(wss)
+  # Verificando se todos os dados são numéricos
+  for(i in 1:ncol(x)) {
+    if (is.numeric(x[,1]) == FALSE) {
+      print('All elements from sample vectors must be numeric')
+    }  
+    break
+  }
+  
+  #Cálculo da variabilidade inicial
+  wss = sum(apply(x, 2, var)) * (nrow(x)-1)
+  #Cálculo das variabilidades geral após a clusterização
+  for(i in 2:20) { wss[i] = sum(kmeans(x, centers = i, iter.max = 500)$withinss) }
+  
+  return(wss)
 }
 wss = Cluster.Number(x)
 
-#Ploting the within square sum of the clusters
-plot(1:length(wss), wss, type = 'b',
-     xlab = 'Number of Clusters', 
-     ylab = 'Within groups sum square',
-     main = 'Number of cluster versus SS within groups',
-     cex.main = 1.2, cex.axis = 0.8, cex.lab = 1)
-grid()
+#Plotando os resultados
+plotaLinhas = function(vetor_de_cluster, subtitulo){
+  
+  # Convertendo o vetor para um dataframe
+  vetor_de_cluster = as.data.frame(vetor_de_cluster)
+  
+  # Criando um vetor de dados para representar o número de clusters
+  clusters = 1:nrow(vetor_de_cluster)
+  vetor_de_cluster$clusters = clusters
+  
+  # Plotando o gráfico
+  ggplot(vetor_de_cluster, aes(x = clusters, y = vetor_de_cluster))+
+    geom_line()+
+    geom_point()+
+    ggtitle("Redução da variabilidade",subtitle = subtitulo)+
+    ylab("Variabilidade")+
+    xlab("Número de Clusters")
+  
+}
 
 #Applying the K-MEANS algorithm
 Kme = kmeans(x, centers = 5, iter.max = 100, 
